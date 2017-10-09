ddc1add343c356074b6527523bfbe1a06bd8b200
Rafael Severo Moreira <rmoreira@ecrmac-100839.local>
2017-10-09 08:51:25 -0300

    Adjusting reducer_books.js

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'JS: The Good Parts', key: 1 },
-        { title: 'Harry Potter', key: 2 },
-        { title: 'The Dark Tower', key: 3 },
-        { title: 'Eloquent Ruby', key: 4 }
+        { title: 'JS: The Good Parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
     ];
 }
